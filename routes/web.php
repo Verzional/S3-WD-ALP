2bfe181e9a2f5d2712b9a4e929d398edbcf40984
Verzional <verzional777@gmail.com>
2024-12-14 14:13:41 +0700

    Resource Routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CompanionController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\SchoolController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +12,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('categories', CategoryController::class);
+Route::resource('companions', CompanionController::class);
+Route::resource('events', EventController::class);
+Route::resource('schedules', ScheduleController::class);
+Route::resource('schools', SchoolController::class);
 Route::resource('students', StudentController::class);
